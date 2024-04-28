418e1e7f56f5ce842e5e3220fbc10c5d0c2f1336
Michael Kulka <michaelkulka28@gmail.com>
2024-04-28 15:12:49 -0400

    added modal to contact us message needs work

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 // script.js
 document.getElementById('estimate').addEventListener('click', function() {
-    alert('Thank you for your interest! We will get back to you shortly.');
-  });
+  document.getElementById("contact").innerHTML = '<!-- Contact Details --> <div class="row"> <div class="col-md-6 text-center mb-3"><h4>Email</h4><p class="text-secondary"><a href="mailto:piantanidahomeservices@gmail.com" class="text-dark">piantanidahomeservices@gmail.com</a></p>                </div><div class="col-md-6 text-center"><h4>Phone</h4><p class="text-secondary">(732) 510-5330</p</div></div>'
+  })
+
+
+
+
+ 
   
